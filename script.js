6c925c85bdc86bea96b054fee423fd564b8404e6
Peter Waterhouse <87525533+peterwaterhouse@users.noreply.github.com>
2024-06-15 15:05:23 +1000

    created function to make row of cells

--- script.js
+++ script.js
@@ -3,16 +3,36 @@ const frame = document.querySelector(".frame");
 
 //find the size of the cell based on the width of the frame
 frameWidth = 500;
-console.log(frameWidth);
 cellAmount = 16;
 cellSize = frameWidth / cellAmount;
 
-const newCell = document.createElement("div");
-newCell.style.display = "flex";
-newCell.style.boxSizing = "border-box";
-newCell.style.border = "2px solid blue";
-newCell.style.flexGrow = "0";
-newCell.style.flexShrink = "0";
-newCell.style.height = (cellSize + "px");
-newCell.style.width = (cellSize + "px");
-frame.appendChild(newCell);
+
+
+// const newCell = document.createElement("div");
+// newCell.style.display = "flex";
+// newCell.style.boxSizing = "border-box";
+// newCell.style.border = "2px solid blue";
+// newCell.style.flexGrow = "0";
+// newCell.style.flexShrink = "0";
+// newCell.style.height = (cellSize + "px");
+// newCell.style.width = (cellSize + "px");
+
+for (i = 0; i < cellAmount; i++) {
+    makeRow();
+    const paragraph = document.createElement("p");
+    frame.appendChild(paragraph);
+}
+
+function makeRow() {
+    for (i = 0; i < cellAmount; i++) {
+        const newCell = document.createElement("div");
+        newCell.style.display = "flex";
+        newCell.style.boxSizing = "border-box";
+        newCell.style.border = "2px solid blue";
+        newCell.style.flexGrow = "0";
+        newCell.style.flexShrink = "0";
+        newCell.style.height = (cellSize + "px");
+        newCell.style.width = (cellSize + "px");
+        frame.appendChild(newCell);
+    }
+}
