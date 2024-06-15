959c9b9c758dceee475812fc7d41f5cefb90c528
Peter Waterhouse <87525533+peterwaterhouse@users.noreply.github.com>
2024-06-15 15:07:47 +1000

    changed row function to fill entire grid and wrap

--- script.js
+++ script.js
@@ -6,25 +6,10 @@ frameWidth = 500;
 cellAmount = 16;
 cellSize = frameWidth / cellAmount;
 
+fillGrid();
 
-
-// const newCell = document.createElement("div");
-// newCell.style.display = "flex";
-// newCell.style.boxSizing = "border-box";
-// newCell.style.border = "2px solid blue";
-// newCell.style.flexGrow = "0";
-// newCell.style.flexShrink = "0";
-// newCell.style.height = (cellSize + "px");
-// newCell.style.width = (cellSize + "px");
-
-for (i = 0; i < cellAmount; i++) {
-    makeRow();
-    const paragraph = document.createElement("p");
-    frame.appendChild(paragraph);
-}
-
-function makeRow() {
-    for (i = 0; i < cellAmount; i++) {
+function fillGrid() {
+    for (i = 0; i < cellAmount*cellAmount; i++) {
         const newCell = document.createElement("div");
         newCell.style.display = "flex";
         newCell.style.boxSizing = "border-box";
